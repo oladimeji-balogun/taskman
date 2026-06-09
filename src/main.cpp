da018daa4a4fcbac5922fddc33a15a58e12da2c9
Oladimeji <boladimeji834@gmail.com>
2026-06-09 19:16:14 +0100

    fix some bug

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include "task.h"
 
 int main() {
-    std::cout << "taskman: up and running\n"; 
+    Task t;
+    t.id = 1;
+    t.title = "Buy groceeries"; 
+    t.due_date = "2026-06-15";
+    t.priority = Priority::High;
+    t.done = false;
+
+    std::boolalpha;
+    std::cout << "ID: " << t.id << "\n"; 
+    std::cout << "Title: " << t.title << "\n"; 
+    std::cout << "Due: " << t.due_date << "\n"; 
+    std::cout << "DOne: " << t.done << "\n";
+
     return 0;
 }
