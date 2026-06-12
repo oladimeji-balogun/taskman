47ed34be60ed62829cec05944a3a8434a3899fa2
Oladimeji <boladimeji834@gmail.com>
2026-06-12 16:36:35 +0100

    test: test the sort and filter features on the command line

--- src/task_manager.cpp
+++ src/task_manager.cpp
@@ -18,12 +18,12 @@ void TaskManager::addTask(const std::string& title, const std::string& due_date,
     tasks.push_back(t);
 }
 
-void TaskManager::listTasks(const std::string& filter = "", const std::string& sort_by = "") const {
+void TaskManager::listTasks(const std::string& filter, const std::string& sort_by) const {
 
     std::vector<Task> result;
 
     // filter operation
-    std::copy_if(tasks.begin(), tasks.end(), std::back_inserter(result), [&filter](Task& t) {
+    std::copy_if(tasks.begin(), tasks.end(), std::back_inserter(result), [&filter](const Task& t) {
         if (filter == "done") return t.done;
         if (filter == "pending") return !t.done;
         return true;
